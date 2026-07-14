d5b7e0bdaea6fadba0bfa9196304004b68485315
Darius Coleman <colemad2@my.erau.edu>
2026-07-14 11:04:12 -0700

    Update: Deletes old cached data when the name changes

--- sw.js
+++ sw.js
@@ -16,6 +16,21 @@ self.addEventListener('install', event => {
   );
 });
 
+// Activate event: clean up old caches
+self.addEventListener('activate', event => {
+  event.waitUntil(
+    caches.keys().then(cacheNames => {
+      return Promise.all(
+        cacheNames.map(cacheName => {
+          if (cacheName !== CACHE_NAME) {
+            console.log('Deleting old cache:', cacheName);
+            return caches.delete(cacheName);
+          }
+        })
+      );
+    })
+  );
+});
 // Fetch event: serve videos from cache
 self.addEventListener('fetch', event => {
   // We only want to handle requests for video files
@@ -24,7 +39,22 @@ self.addEventListener('fetch', event => {
       caches.match(event.request)
         .then(response => {
           return response || fetch(event.request);
+      caches.match(event.request).then(cachedResponse => {
+        // If the video is in the cache, return it.
+        if (cachedResponse) {
+          return cachedResponse;
+        }
+
+        // If not, fetch it from the network, cache it, and then return it.
+        return caches.open(CACHE_NAME).then(cache => {
+          return fetch(event.request).then(networkResponse => {
+            // Clone the response because it's a stream and can only be consumed once.
+            cache.put(event.request, networkResponse.clone());
+            return networkResponse;
+          });
         })
     );
+      })
+    )
   }
 });
